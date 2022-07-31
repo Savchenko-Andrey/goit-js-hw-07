11430a8f2ba75e05359b3a943d62c19cf28f0bae
Savchenko-Andrey <95830161+Savchenko-Andrey@users.noreply.github.com>
2022-07-31 22:39:24 +0300

    change 1

---
+++ js/02-lightbox.js
@@ -0,0 +1,36 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galery = document.querySelector(".gallery");
+
+const onCreateGalery = (large, small, alt) => {
+  return `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${large}">
+        <img
+            class="gallery__image"
+            src="${small}"
+            alt="${alt}"
+        />
+    </a>
+</div>`;
+};
+
+const calcGalery = galleryItems
+  .map((item) => {
+    return onCreateGalery(item.original, item.preview, item.description);
+  })
+  .join("");
+
+galery.insertAdjacentHTML("afterbegin", calcGalery);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+gallery.on("show.simplelightbox");
+
